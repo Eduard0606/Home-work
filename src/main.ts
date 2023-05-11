107c70c31f02bb7da5cfdfb7ed3ec4d0eeb6acb4
Eduard0606 <zalyaev2001@list.ru>
2023-05-11 19:22:02 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,38 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import './style.css'
+// 1. Запросить у пользователя его возраст и определить, кем он 
+// является: ребенком (0–2), подростком (12–18), взрослым 
+// (18_60) или пенсионером (60– ...).
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// let user = +prompt("Введите возраст")
+// if (user >=0 && user <=11){
+//     console.log("Приветик")
+// }
+// else if (user >= 12 && user <=18){
+//     console.log("Привет")
+// }
+// else if (user >=19 && user <=60){
+//     console.log("Здравствуйте")
+// }
+// else if (user >=60 && user <=100){
+//     console.log("Здравствуйте, уважаемый")
+// }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+// 2. Запросить у пользователя число от 0 до 9 и вывести ему 
+// спецсимвол, который расположен на этой клавише (1–!, 
+// 2–@, 3–# и т. д).
+// let user2 =+ prompt("Введите число от 1 до 9")
+// if (user2 == 1) console.log("!") 
+// else if (user2 ==2) console.log("#")
+// else if (user2 ==3) console.log("$")
+// else if (user2 ==4) console.log("?")
+// else if (user2 ==5) console.log(")")
+// else if (user2 ==6) console.log("^")
+// else if (user2 ==7) console.log("&")
+// else if (user2 ==8) console.log("*")
+// else if (user2 ==9) console.log("(")
+
+// 3. Запросить у пользователя трехзначное и число и проверить, 
+// есть ли в нем одинаковые цифры.
+let user3 = prompt("Введите трехзначное число")
+if 
