99d6a6ebecdf2f030bf10333abef0c34c8d118aa
Your Name <you@example.com>
2023-06-29 15:34:32 +0300

    Калькулятор

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,7 @@
-// import './style.css'
-// 1. Запросить у пользователя его возраст и определить, кем он 
-// является: ребенком (0–2), подростком (12–18), взрослым 
-// (18_60) или пенсионером (60– ...).
-
-// let user = +prompt("Введите возраст")
-// if (user >=0 && user <=11){
-//     console.log("Приветик")
-// }
-// else if (user >= 12 && user <=18){
-//     console.log("Привет")
-// }
-// else if (user >=19 && user <=60){
-//     console.log("Здравствуйте")
-// }
-// else if (user >=60 && user <=100){
-//     console.log("Здравствуйте, уважаемый")
-// }
-
-
-// 2. Запросить у пользователя число от 0 до 9 и вывести ему 
-// спецсимвол, который расположен на этой клавише (1–!, 
-// 2–@, 3–# и т. д).
-// let user2 =+ prompt("Введите число от 1 до 9")
-// if (user2 == 1) console.log("!") 
-// else if (user2 ==2) console.log("#")
-// else if (user2 ==3) console.log("$")
-// else if (user2 ==4) console.log("?")
-// else if (user2 ==5) console.log(")")
-// else if (user2 ==6) console.log("^")
-// else if (user2 ==7) console.log("&")
-// else if (user2 ==8) console.log("*")
-// else if (user2 ==9) console.log("(")
-
-// 3. Запросить у пользователя трехзначное и число и проверить, 
-// есть ли в нем одинаковые цифры.
-let user3 = prompt("Введите трехзначное число")
-if 
+const objectA = {
+    a:10,
+    b:true  
+}
+const copyOfA = objectA
+copyOfA.a = 30
+console.log(objectA )
